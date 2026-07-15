b72315c1f6b1592f5a543fae9bab57d43de799ef
nahwoah <119922465+nahwoah@users.noreply.github.com>
2026-07-15 15:44:57 +0300

    added some structure from the library project

--- src/index.js
+++ src/index.js
@@ -1,24 +1,41 @@
 import "./style.css";
 
+const table = document.querySelector("table");
+const tbody = document.querySelector("tbody");
+const newitem = document.getElementById('new-item');
+const submit = newitem.querySelector('#submitBtn');
+const add_title = newitem.querySelector('#add-title');
+const add_description = newitem.querySelector('#add-description');
+const add_notes = newitem.querySelector('#add-notes');
+const add_read = newitem.querySelector('#add-read');
+
 class List{
     constructor(title, description){
+        this.id = crypto.randomUUID();
         this.title = title;
         this.description = description;
     }
 }
 
-let jsonObject = []
+const toDoList = []
 
-let obj = new List(prompt("Enter title"), prompt("Enter a desc"))
+function addToDoItem() {
+    const item = new List(add_title.value, add_description.value, add_notes.value, add_read.checked)
 
-jsonObject.push(obj)
+    toDoList.push(item);
 
-let obj2 = new List(prompt("Enter title"), prompt("Enter a desc"))
-jsonObject.push(obj2)
+}
 
-let jsonString = JSON.stringify(jsonObject)
+console.log(toDoList)
 
-console.log("object", jsonObject)
-console.log("stringified", jsonString)
+// let obj = new List(prompt("Enter title"), prompt("Enter a desc"))
+// jsonObject.push(obj)
+
+
+
+// let jsonString = JSON.stringify(jsonObject)
+
+// console.log("object", jsonObject)
+// console.log("stringified", jsonString)
 
 
