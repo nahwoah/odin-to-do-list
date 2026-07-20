917244848b215a25c33c7b9f048fb3b4310e2790
nahwoah <119922465+nahwoah@users.noreply.github.com>
2026-07-20 15:50:05 +0300

    set div items in a grid and changed to drawing divs instead of a table. Changed the style a bit too

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const add_title = newitem.querySelector('#add-title');
 const add_description = newitem.querySelector('#add-description');
 const add_notes = newitem.querySelector('#add-notes');
 const add_completed = newitem.querySelector('#add-completed');
+const container = document.querySelector("#container");
 
 
 class List {
@@ -56,18 +57,23 @@ function addToTable() {
             if (idx === List.length) {
                 const tdCompleted = document.createElement('td');
                 const completedBtn = document.createElement('button');
+                completedBtn.style.height = '40px'
+                completedBtn.style.width = '40px'
+                completedBtn.style.background = 'transparent'
+                completedBtn.style.border = '2px solid white'
+                completedBtn.style.borderRadius = '50%'
                 if (!item.completed) {
-                    completedBtn.textContent = '❌ ' + item[key];
+                    completedBtn.textContent = '❌';
                 } else {
-                    completedBtn.textContent = '✅ ' + item[key];
+                    completedBtn.textContent = '✔️';
                 }
 
                 completedBtn.onclick = function () {
                     item.changeCompleted();
                     if (item.completed) {
-                        completedBtn.textContent = '✅ ' + item[key];
+                        completedBtn.textContent = '✔️';
                     } else {
-                        completedBtn.textContent = '❌ ' + item[key];
+                        completedBtn.textContent = '❌';
                     }
 
                 }
@@ -105,12 +111,47 @@ function addToTable() {
     })
 }
 
+function drawItem(){
+
+    const item = new List(add_title.value, add_description.value, add_notes.value)
+
+    toDoList.push(item);
+
+    // addToDoItem()
+    const itemdiv = document.createElement("div");
+    itemdiv.id = "itemdiv"
+
+    const icon = document.createElement("button")
+    icon.id = 'icon'
+    const title = document.createElement("span");
+    const desc = document.createElement("span");
+    const notes = document.createElement("span")
+
+    // icon.textContent = '◯'
+
+    icon.addEventListener('mouseover', e => {
+        icon.textContent = '✓' //  fallback   ✓ ✔️
+        icon.style.color = 'aquamarine'
+    })
+    icon.addEventListener('mouseleave', e =>{
+        icon.textContent = ''
+    })
+
+    title.textContent = `${item.title} `;
+    desc.textContent = `${item.description} `;
+    notes.textContent = `${item.notes} `;
+
+    itemdiv.append(icon, title, desc, notes);
+    container.appendChild(itemdiv);
+}
+
 submit.addEventListener("click", function (e) {
 
     e.preventDefault()
 
-    addToDoItem();
-    addToTable();
+    // addToDoItem();
+    // addToTable();
+    drawItem()
 
     newitem.close();
     add_title.value = '';
